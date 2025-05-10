9612bdb5a55a307d9c30fbe31a22d19bd210396a
Fayche <1751162157@qq.com>
2025-05-10 20:13:39 +0800

    refactor: Rename variable for clarity and improve code formatting

--- src/fetch_custom_node_list.py
+++ src/fetch_custom_node_list.py
@@ -1,17 +1,20 @@
 from utils import fetch_json, save_json
 
-COMFYUI_MANAGER_LIST_URL = "https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/custom-node-list.json"
+CUSTOM_NODE_LIST_URL = "https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/custom-node-list.json"
 
 
 def fetch_custom_node_list():
-    return fetch_json(COMFYUI_MANAGER_LIST_URL)
+    """Fetches the custom node list from the specified URL."""
+    return fetch_json(CUSTOM_NODE_LIST_URL)
 
 
 def save_custom_node_list():
+    """Fetches the custom node list and saves it to a local JSON file."""
     data = fetch_custom_node_list()
-    save_json(data, 'src/data/custom-node-list.json')
+    save_json(data, "src/data/custom-node-list.json")
     print("custom-node-list.json saved.")
 
 
 if __name__ == "__main__":
+    # Main execution block: saves the custom node list when the script is run directly.
     save_custom_node_list()
